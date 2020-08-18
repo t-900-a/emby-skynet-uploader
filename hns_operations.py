1b05228fef3a6efb390a51be36324569498f65a7
t-900-a <t-900@tutanota.com>
2020-08-18 09:27:17 -0400

    Now using records list instead of variables for update_namebase_dns() Now conforming to api spec

--- hns_operations.py
+++ hns_operations.py
@@ -1,25 +1,32 @@
 from namebase_exchange.exchange import *
 
 def update_namebase_dns(api_key: str, secret_key: str, domain: str, sialink : str):
+    updated_records = []
+    # chop off the sia://
+    sialink = sialink[6:]
     exchange = Exchange(api_key, secret_key)
     dns_settings = exchange.get_dns_settings(domain)
     dns_records = dns_settings['records']
     ns_set = False
-    # verify a nameserver is already specified
+    txt_set = False
     for record in dns_records:
+        # verify a nameserver is already specified
         if record['type'] == 'NS' and record['value'] != '':
             ns_set = True
+        # modify the existing TXT record if there is one
+        if record['type'] == 'TXT':
+            txt_set = True
+            record['host'] = '@'
+            record['value'] = sialink
+        updated_records.append(record)
 
     if ns_set == False:
         # if there is no nameserver add one
         # name server ip address from blog: https://www.namebase.io/blog/setting-dns-records/
-        exchange.update_dns_settings(domain=domain,
-                                     record_type='NS',
-                                     host='ns1',
-                                     value='44.231.6.183')
-    # chop off the sia://
-    sialink = sialink[6:]
+        updated_records.append({'type':'NS','host':'ns1','value':'44.231.6.183','ttl':0})
+    if txt_set == False:
+        updated_records.append({'type':'TXT','host':'',
+                                'value':'AAApJJPnci_CzFnddB076HGu1_C64T6bfoiQqvsiVB5XeQ','ttl':0})
+
     exchange.update_dns_settings(domain=domain,
-                                 record_type='TXT',
-                                 host='@',
-                                 value=sialink, ttl=0)
+                                 records=updated_records)
